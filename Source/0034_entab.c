ef9d6f74f5f4ed699be80b2b646249abd92784b0
weenchvd <d88744@gmail.com>
2018-09-13 22:11:47 +0300

    Chapter 1.10 | Exercise 1.21

--- Source/0034_entab.c
+++ Source/0034_entab.c
@@ -1,5 +1,4 @@
-/** Exercise 1.21
- * not finished TODO*/
+/** Exercise 1.21 */
 
 #include <stdio.h>
 
@@ -14,6 +13,7 @@ int main()
     char line[MAXLINE]; /* current line */
     printf("\nUse \"New Line + `` + Enter\" to output the result\n");
     while((len = mygetline(line, MAXLINE)) > 0) {
+        //int nt = 0, ns = 0;
         for(int i = 0, c = 0; i < len; i++) {
             if(line[i] == ' ' && line[i + 1] == ' ') {
                 c++;
@@ -21,17 +21,24 @@ int main()
             else if(line[i] == ' ' && c > 0) {
                 c++;
                 int s, t, k;
-                if((k = ((i + 1) / LENGTHTAB) * LENGTHTAB - (i - c + 1)) > 0) {
-                    t = k / LENGTHTAB + 1;
-                    s = (i + 1) - ((i + 1) / LENGTHTAB) * LENGTHTAB;
+                if((k = ((i + 1) / LENGTHTAB) * LENGTHTAB) > (i + 1 - c)) {
+                    if((i + 1 - c) - ((i + 1 - c) / LENGTHTAB) * LENGTHTAB == 0) {
+                        t = (k - (i + 1 - c)) / LENGTHTAB;
+                    }
+                    else {
+                        t = (k - (i + 1 - c)) / LENGTHTAB + 1;
+                    }
+                    s = (i + 1) - k;
+                    //nt = nt + t;
                     while(t > 0) {
                         printf("\t");
                         t--;
                     }
                 }
                 else {
-                    s = (i + 1) - c;
+                    s = c;
                 }
+                //ns = ns + s;
                 while(s > 0) {
                     printf(" ");
                     s--;
@@ -42,6 +49,7 @@ int main()
                 printf("%c", line[i]);
             }
         }
+        //printf("\nNumbers of TABS: %4d\nNumbers of SPACES (exclude single spaces): %4d\n", nt, ns);
     }
     return 0;
 }
