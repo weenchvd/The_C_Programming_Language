75024e74eba2c1ac8b355216a0bc26c976f3334c
weenchvd <d88744@gmail.com>
2018-09-15 20:10:48 +0300

    Chapter 1.10 | Exercise 1.23 (draft)

--- Source/0036_del_comment.c
+++ Source/0036_del_comment.c
@@ -3,17 +3,42 @@
 
 #include <stdio.h>
 
-#define SIZE 5000
+#define SIZE 100000
+#define TRUE 1
+#define FALSE 0
 
 int getallchar(char m_text[], int size);
+int getposition(char m_text[], int currentcarriageposition);
+
+int cpos, isquotes, iscomment1, iscommentn;
 
 int main()
 {
     printf("Use \'New line\' + \"``\" + \"Enter\" to output the result\n");
     int len;
+    cpos = -1;
+    isquotes = FALSE;
+    iscomment1 = FALSE;
+    iscommentn = FALSE;
     char m_text[SIZE];
     len = getallchar(m_text, SIZE);
-
+    for(int i = 0; i < len; i++) {
+        if(isquotes == FALSE && m_text[i] == '/' && m_text[i + 1] == '*') {
+            iscommentn = TRUE;
+            i = getposition(m_text, i);
+        }
+        else if(isquotes == FALSE && m_text[i] == '/' && m_text[i + 1] == '/') {
+            iscomment1 = TRUE;
+            i = getposition(m_text, i);
+        }
+        else if(isquotes == FALSE && m_text[i] == '\"' && m_text[i - 1] != '\\') {
+            isquotes = TRUE;
+        }
+        else if(isquotes == TRUE && m_text[i] == '\"' && m_text[i - 1] != '\\') {
+            isquotes = FALSE;
+        }
+        printf("%c", m_text[i]);
+    }
     return 0;
 }
 
@@ -21,7 +46,7 @@ int getallchar(char m_text[], int size)
 {
     int i, c;
     for(i = 0; i < size && (c = getchar()) != EOF; i++) {
-        if(c == ' ' && m_text[i - 1] == ' ' && m_text[i - 2] == '\n') {
+        if(c == '`' && m_text[i - 1] == '`' && m_text[i - 2] == '\n') {
             m_text[i - 1] = '\0';
             return (i - 1);
         }
@@ -29,4 +54,9 @@ int getallchar(char m_text[], int size)
             m_text[i] = c;
         }
     }
+}
+
+int getposition(char m_text[], int ccpos)
+{
+    return;
 }
