71f94180024307041612502683dcdcaf939e02c1
weenchvd <d88744@gmail.com>
2018-09-30 02:03:51 +0300

    Chapter 2.2 | Exercise 2.1 (draft)

--- Source/0101_ranges.c
+++ Source/0101_ranges.c
@@ -5,9 +5,10 @@
 #include <limits.h>
 #include <float.h>
 
-long long int exponentiation(long long int x, int y); /** x^y */
-unsigned long long int exponentiationU(unsigned long long int x, int y); /** x^y */
-long double exponentiationF(long double x, int y); /** x^y */
+long long int expon(long long int x, int y); /** x^y */
+unsigned long long int exponU(unsigned long long int x, int y); /** x^y */
+long double exponF(long double x, int y); /** x^y */
+long double exponFMaxPos(long double x, int y, int mantissa); /** x^y */
 
 int main()
 {
@@ -32,28 +33,26 @@ int main()
     printf("----------------------------------------------------------------------------------------\n");
     printf("Ranges calculated\n");
     printf("Range SIGNED CHAR:             from %22lli    to %22lli\n",
-            - exponentiation(2, sizeof(char) * 8) / 2, exponentiation(2, sizeof(char) * 8) / 2 - 1);
+            - expon(2, sizeof(char) * 8) / 2, expon(2, sizeof(char) * 8) / 2 - 1);
     printf("Range SIGNED SHORT INT:        from %22lli    to %22lli\n",
-           - exponentiation(2, sizeof(short int) * 8) / 2, exponentiation(2, sizeof(short int) * 8) / 2 - 1);
+           - expon(2, sizeof(short int) * 8) / 2, expon(2, sizeof(short int) * 8) / 2 - 1);
     printf("Range SIGNED INT:              from %22lli    to %22lli\n",
-           - exponentiation(2, sizeof(int) * 8) / 2, exponentiation(2, sizeof(int) * 8) / 2 - 1);
+           - expon(2, sizeof(int) * 8) / 2, expon(2, sizeof(int) * 8) / 2 - 1);
     printf("Range SIGNED LONG INT:         from %22lli    to %22lli\n",
-           - (long long int) exponentiationU(2, sizeof(long int) * 8 - 1),
-           (long long int) (exponentiationU(2, sizeof(long int) * 8 - 1) - 1));
+           - (long long int) exponU(2, sizeof(long int) * 8 - 1),
+           (long long int) (exponU(2, sizeof(long int) * 8 - 1) - 1));
     printf("Range SIGNED LONG LONG INT:    from %22lli    to %22lli\n",
-           - (long long int) exponentiationU(2, sizeof(long long int) * 8 - 1),
-           (long long int) (exponentiationU(2, sizeof(long long int) * 8 - 1) - 1));
+           - (long long int) exponU(2, sizeof(long long int) * 8 - 1),
+           (long long int) (exponU(2, sizeof(long long int) * 8 - 1) - 1));
     /*printf("1_____SIGNED LONG LONG INT:    from -%llu to -%llu\n",
-           exponentiationU(2, sizeof(long long int) * 8) / 2,
-           exponentiationU(2, sizeof(long long int) * 8) / 2 - 1);*/
+           exponU(2, sizeof(long long int) * 8) / 2, exponU(2, sizeof(long long int) * 8) / 2 - 1);*/
     /*printf("2_____SIGNED LONG LONG INT:    from -%llu to -%llu\n",
-           (exponentiationU(2, sizeof(long long int) * 8) / 2ULL),
-           (exponentiationU(2, sizeof(long long int) * 8) / 2ULL - 1));*/
-    printf("Range UNSIGNED CHAR:           from %22u    to %22llu\n", 0, exponentiationU(2, sizeof(char) * 8) - 1);
-    printf("Range UNSIGNED SHORT INT:      from %22u    to %22llu\n", 0, exponentiationU(2, sizeof(short int) * 8) - 1);
-    printf("Range UNSIGNED INT:            from %22u    to %22llu\n", 0, exponentiationU(2, sizeof(int) * 8) - 1);
-    printf("Range UNSIGNED LONG INT:       from %22u    to %22llu\n", 0, exponentiationU(2, sizeof(long int) * 8) - 1);
-    printf("Range UNSIGNED LONG LONG INT:  from %22u    to %22llu\n", 0, exponentiationU(2, sizeof(long long int) * 8) - 1);
+           exponU(2, sizeof(long long int) * 8) / 2ULL, exponU(2, sizeof(long long int) * 8) / 2ULL - 1);*/
+    printf("Range UNSIGNED CHAR:           from %22u    to %22llu\n", 0, exponU(2, sizeof(char) * 8) - 1);
+    printf("Range UNSIGNED SHORT INT:      from %22u    to %22llu\n", 0, exponU(2, sizeof(short int) * 8) - 1);
+    printf("Range UNSIGNED INT:            from %22u    to %22llu\n", 0, exponU(2, sizeof(int) * 8) - 1);
+    printf("Range UNSIGNED LONG INT:       from %22u    to %22llu\n", 0, exponU(2, sizeof(long int) * 8) - 1);
+    printf("Range UNSIGNED LONG LONG INT:  from %22u    to %22llu\n", 0, exponU(2, sizeof(long long int) * 8) - 1);
     printf("========================================================================================\n");
     printf("FLOAT, bytes:          %3lu\n", sizeof(float));
     printf("DOUBLE, bytes:         %3lu\n", sizeof(double));
@@ -61,44 +60,59 @@ int main()
     printf("----------------------------------------------------------------------------------------\n");
     printf("Ranges from \"float.h\"\n");
     printf("Range FLOAT:                   from %22g    to %22g\n", FLT_MIN, FLT_MAX);
-    /*printf("Range FLOAT: from %.50f\n"
-           "             to %f\n", FLT_MIN, FLT_MAX);*/
     printf("Range DOUBLE:                  from %22g    to %22g\n", DBL_MIN, DBL_MAX);
     printf("Range LONG DOUBLE:             from %22Lg    to %22Lg\n", LDBL_MIN, LDBL_MAX);
+    /*printf("Range: from %.44f\n       to   %f\n", FLT_MIN, FLT_MAX);*/
     printf("----------------------------------------------------------------------------------------\n");
     printf("Ranges calculated\n");
     printf("Range FLOAT:                   from %22Lg    to %22Lg\n",
-           1.0 / exponentiationF(2.0, (int) (exponentiationU(2, 7) - 2)), 0.0L);
+            1.0 / exponF(2.0, 126), exponFMaxPos(2.0, 127, 24));
+    printf("Range DOUBLE:                  from %22Lg    to %22Lg\n",
+           1.0 / exponF(2.0, 1022), exponFMaxPos(2.0, 1023, 53));
+    printf("Range LONG DOUBLE:             from %22Lg    to %22Lg\n",
+           1.0 / exponF(2.0, 16382), exponFMaxPos(2.0, 16383, 113));
+    /*printf("Range: from %.44f\n       to   %f\n",
+            (double) (1.0 / exponF(2.0, 126)), (double) exponFMaxPos(2.0, 127, 24));*/
     printf("========================================================================================\n");
     return 0;
 }
 
-long long int exponentiation(long long int x, int y)
+long long int expon(long long int x, int y)
 {
     if(y == 0) {
         return 1;
     }
     else {
-        return x * exponentiation(x, y - 1);
+        return x * expon(x, y - 1);
     }
 }
 
-unsigned long long int exponentiationU(unsigned long long int x, int y)
+unsigned long long int exponU(unsigned long long int x, int y)
 {
     if(y == 0) {
         return 1;
     }
     else {
-        return x * exponentiationU(x, y - 1);
+        return x * exponU(x, y - 1);
     }
 }
 
-long double exponentiationF(long double x, int y)
+long double exponF(long double x, int y)
 {
     if(y == 0) {
         return 1.0L;
     }
     else {
-        return x * exponentiationF(x, y - 1);
+        return x * exponF(x, y - 1);
+    }
+}
+
+long double exponFMaxPos(long double x, int y, int m)
+{
+    if(m > 0) {
+        return exponF(x, y) + exponFMaxPos(x, y - 1, m - 1);
+    }
+    else {
+        return 0.0L;
     }
 }
